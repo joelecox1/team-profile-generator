afbee27347ae8a21242cb808bf1289f90705e389
Count Zero <joelecox1@gmail.com>
2021-02-28 20:16:24 -0600

    manager tests

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,24 @@
+const { test, expect } = require('@jest/globals');
+const Employee = require('../lib/Employee');
+const Manager = require('../lib/Manager');
+
+test('if office number can be set via constructor argument', () => {
+  const testValue = 100;
+  const e = new Manager('foo', 3, 'test@test.com', testValue);
+
+  expect(e.officeNumber).toBe(testValue);
+});
+
+test('if getRole returns manager', () => {
+  const testValue = 'Manager';
+  const e = new Manager('foo', 3, 'test@test.com', 100);
+
+  expect(e.getRole()).toBe(testValue);
+});
+
+test('if you can get office number via office function', () => {
+  const testValue = 100;
+  const e = new Manager('foo', 3, 'test@test.com', testValue);
+
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
